d760b2fc7d9f927bc3acd039ef24f293eb92929a
Philipp Eder <p.eder92@gmx.at>
2026-02-20 18:08:13 +0100

    try new gh pages build

--- scully.carmenquartett.config.ts
+++ scully.carmenquartett.config.ts
@@ -6,6 +6,7 @@ import '@scullyio/scully-plugin-puppeteer'
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "carmenquartett",
+  distFolder: './dist/carmenquartett',
   // add spsModulePath when using de Scully Platform Server,
   outDir: './docs',
   routes: {
